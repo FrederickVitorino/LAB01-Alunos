081e070249e50049348444d8648384bf0e969469
Frederick Vitorino <frederickvitorino2@hotmail.com>
2025-03-27 19:31:31 -0300

    Exercício 1

---
+++ Exercício 1/script.js
@@ -0,0 +1,45 @@
+function cadastrar(){
+    const valores = ['nome', 'idade', 'curso', 'NF'];
+    const aluno = {nome: '', idade: '', curso: '', NF: ''};
+
+    valores.forEach((valor, _) => {
+        getValor = document.getElementById(valor).value;
+        aluno[valor] = getValor;
+    });
+
+    addAlunoTabela(aluno);
+}
+
+function addAlunoTabela(aluno){
+    const tabela = document.getElementById('tabela');
+    const linha = tabela.insertRow(1);
+
+    const colunas = ['nome', 'idade', 'curso', 'NF'];
+    colunas.forEach((coluna, index) => {
+        const celula = linha.insertCell(index);
+        celula.innerHTML = aluno[coluna];
+    });
+
+    const botoes = [
+        { texto: 'Editar', evento: () => editarAluno(linha) },
+        { texto: 'Excluir', evento: () => excluirAluno(linha) }
+    ];
+
+    botoes.forEach((comando, index) => {
+        const celula = linha.insertCell(colunas.length + index);
+        const botao = document.createElement('button');
+        botao.type = 'button';
+        botao.innerHTML = comando.texto;
+        botao.onclick = comando.evento;
+        celula.appendChild(botao);
+    });
+}
+
+function editarAluno(linha){
+    return true;
+}
+
+function excluirAluno(linha){
+    return true;
+}
+
